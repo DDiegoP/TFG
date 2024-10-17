a27830760df358b45e87037c0568e4c016f0caa6
Diego López Balboa <diegol08@ucm.es>
2024-10-18 01:54:58 +0200

    Con puerto vinculado y continua sin ir bien :/

--- pruebas/godot/porttest/sender.mjs
+++ pruebas/godot/porttest/sender.mjs
@@ -3,12 +3,18 @@ import dgram from 'dgram';
 const client = dgram.createSocket('udp4');
 
 //Ip server
-const serverIP = '192.168.1.42'; 
+const serverIP = '192.168.1.40'; 
 const serverPort = 3000; 
 
-// Mensaje
-const message = Buffer.from('/play');
+const localPort = 9000
 
+// Mensaje
+const message = Buffer.from('/play', 'utf8');
+console.log('Mensaje a enviar:', message.toString());
+
+client.bind(localPort, () => {
+  console.log(`Vinculado al puerto ${localPort}`);
+});
 // Envía el mensaje al servidor
 client.send(message, 0, message.length, serverPort, serverIP, (err) => {
   if (err) {
