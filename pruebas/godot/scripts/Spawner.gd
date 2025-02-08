f98f0a4799723ebe5ce5dc1056480dea0c65b165
Diego López Balboa <diegol08@ucm.es>
2025-02-08 12:33:34 +0100

    Array de notas por cuerda

--- pruebas/godot/scripts/Spawner.gd
+++ pruebas/godot/scripts/Spawner.gd
@@ -1,11 +1,26 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var counter = 0
+@export var string1 : Marker2D
+@export var string1Notes = []
+@export var string2 : Marker2D
+@export var string2Notes = []
+@export var string3 : Marker2D
+@export var string3Notes = []
+@export var string4 : Marker2D
+@export var string4Notes = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(string1Notes.size() > 0):
+		string1.spawnNote(string1Notes[0])
+		string1Notes.remove_at(0)
+	if(string2Notes.size() > 0):
+		string2.spawnNote(string2Notes[0])
+		string2Notes.remove_at(0)
+	if(string3Notes.size() > 0):
+		string3.spawnNote(string3Notes[0])
+		string3Notes.remove_at(0)
+	if(string4Notes.size() > 0):
+		string4.spawnNote(string4Notes[0])
+		string4Notes.remove_at(0)
--- pruebas/godot/scripts/StringButton.gd
+++ pruebas/godot/scripts/StringButton.gd
@@ -3,17 +3,10 @@ extends Node
 @export var nota: PackedScene 
 @export var button: Button
 @export var key = ""
-var rng = RandomNumberGenerator.new()
-var randomSpawnTime = 0
 
-func _ready():
-	randomSpawnTime = rng.randf_range(5, 12)
 
-func _process(delta):
-	randomSpawnTime -= delta;
-	if(randomSpawnTime<0):
-		var newNote = nota.instantiate()
-		add_child(newNote)
-		newNote.key = key
-		button.pressed.connect(newNote._on_boton_pressed)
-		randomSpawnTime = rng.randf_range(5,15)
+func spawnNote(note):
+	var newNote = nota.instantiate()
+	add_child(newNote)
+	newNote.key = note
+	button.pressed.connect(newNote._on_boton_pressed)
