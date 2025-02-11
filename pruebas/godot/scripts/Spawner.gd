8ea9ee38a88ed936b14a07d1ff41be6ac8e1facb
Diego López Balboa <diegol08@ucm.es>
2025-02-11 20:15:36 +0100

    Minijuego con timeStamps

--- pruebas/godot/scripts/Spawner.gd
+++ pruebas/godot/scripts/Spawner.gd
@@ -1,26 +1,20 @@
 extends Node
 
-var counter = 0
-@export var string1 : Marker2D
-@export var string1Notes = []
-@export var string2 : Marker2D
-@export var string2Notes = []
-@export var string3 : Marker2D
-@export var string3Notes = []
-@export var string4 : Marker2D
-@export var string4Notes = []
+var timeStamp = 0
+var currentIndex = 0
+@export var bassStrings: Array[Marker2D] = []
+@export var stringNotes = []
+@export var noteStamps = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(string1Notes.size() > 0):
-		string1.spawnNote(string1Notes[0])
-		string1Notes.remove_at(0)
-	if(string2Notes.size() > 0):
-		string2.spawnNote(string2Notes[0])
-		string2Notes.remove_at(0)
-	if(string3Notes.size() > 0):
-		string3.spawnNote(string3Notes[0])
-		string3Notes.remove_at(0)
-	if(string4Notes.size() > 0):
-		string4.spawnNote(string4Notes[0])
-		string4Notes.remove_at(0)
+	timeStamp += delta
+	if(noteStamps[currentIndex] < timeStamp):
+		var chosen = randi_range(0,3)
+		bassStrings[chosen].spawnNote(stringNotes[currentIndex])
+		if(currentIndex + 1 == noteStamps.size() || currentIndex + 1 == stringNotes.size()):
+			currentIndex = 0
+			timeStamp = 0
+		else:
+			currentIndex += 1
+	
