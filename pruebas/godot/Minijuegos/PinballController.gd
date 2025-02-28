987821afa9541208d997cde2a3fd0053625baf6e
Diego López Balboa <diegol08@ucm.es>
2025-02-28 20:01:48 +0100

    Paddles

--- pruebas/godot/Minijuegos/PinballController.gd
+++ pruebas/godot/Minijuegos/PinballController.gd
@@ -2,13 +2,34 @@ extends Node2D
 
 @export var ButtonLeft : Button
 @export var ButtonRight : Button
-@export var Pala1 : StaticBody2D
-@export var Pala2 : StaticBody2D
+@export var PaddleL : RigidBody2D
+@export var PaddleR : RigidBody2D
+
+@export var turnDegrees = 45
+@export var turnSpeed = 10
+var initDegreesL
+var initDegreesR
+var targetDegreesL
+var targetDegreesR
 
 func _ready():
-	pass # Replace with function body.
+	initDegreesL = PaddleL.rotation
+	initDegreesR = PaddleR.rotation
+	targetDegreesL = initDegreesL
+	targetDegreesR = initDegreesR
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	PaddleL.set_rotation(move_toward(PaddleL.rotation, targetDegreesL, delta * turnSpeed))
+	PaddleR.set_rotation(move_toward(PaddleR.rotation, targetDegreesR, delta * turnSpeed))
+
+func _on_button_left_button_down():
+	targetDegreesL = initDegreesL + deg_to_rad(-turnDegrees)
+
+func _on_button_right_button_down():
+	targetDegreesR = initDegreesR + deg_to_rad(turnDegrees)
+
+func _on_button_left_button_up():
+	targetDegreesL = initDegreesL
+
+func _on_button_right_button_up():
+	targetDegreesR = initDegreesR
