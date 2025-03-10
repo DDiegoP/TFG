43138230f2943b6674131097c5a3e0b4bcf5517c
Diego López Balboa <diegol08@ucm.es>
2025-03-10 16:36:24 +0100

    Gestos de movil y comentarios

--- pruebas/godot/Minijuegos/MinijuegoGatoRodante.gd
+++ pruebas/godot/Minijuegos/MinijuegoGatoRodante.gd
@@ -1,31 +1,35 @@
 extends Node2D
 
+#Bola sobre la que se aplica la fuerza
 @export var ball : RigidBody2D
+#Valor de fuerza que se aplica a la vola
 @export var ballSpeed : float
 
+#Fuerza que se esta aplicando en la bola
 var forceToApply : Vector2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ball.apply_central_impulse(forceToApply)
+	var accelerForce = Input.get_accelerometer()
+	ball.apply_central_impulse(forceToApply+ Vector2(accelerForce.x, 0))
 
-
+#Se cambia el vector de fuerza horizontal que se aplica a el nuevo valor
 func changeForce(newForce):
 	forceToApply = Vector2(newForce, 0)
 
-
+#Al pulsar el boton izquierdo se añade fuerza a la izquierda
 func _on_button_left_button_down():
 	changeForce(-ballSpeed)
 
-
+#Al levantar el boton izquierdo se añade elimina la fuerza
 func _on_button_left_button_up():
 	changeForce(0)
 
-
+#Al pulsar el boton derecho se añade fuerza a la derecha
 func _on_button_right_button_down():
 	changeForce(ballSpeed)
 
-
+#Al levantar el boton derecho se añade elimina la fuerza
 func _on_button_right_button_up():
 	changeForce(0)
--- pruebas/godot/Scripts/SubwaySurfersPlayer.gd
+++ pruebas/godot/Scripts/SubwaySurfersPlayer.gd
@@ -4,28 +4,41 @@ extends RigidBody3D
 @export var lerpSpeed = 10
 #Posiciones designadas del jugador
 @export var playerPositions: Array[Marker3D] = []
-#Boton izquierdo
-@export var leftButton : Button
-#Boton derecho
-@export var rightButton : Button
 #Indice de posicion del player
 @export var currentIndex = 0
 #Posicion inicial del player
 var initialPos
 #Posicion a la que el jugador tiene que desplazarse
 var targetPos
+#Posicion inicial en la que el usuario pulsa en pantalla
+var initSwipePos
+#Posicion en la que el usuario termina de pulsar en pantalla
+var finalSwipePos
 
 # Se conectan las señales y se setea la posicion del jugador a la del indice puesto
 func _ready():
-	leftButton.connect("pressed",moveLeft)
-	rightButton.connect("pressed",moveRight)
 	targetPos = playerPositions[currentIndex]
 	initialPos = currentIndex
 	position = targetPos.global_position
+	
+
+func _unhandled_input(event):
+	if(event.is_action_pressed("LMB")):
+		initSwipePos = event.get_position()
+	if(event.is_action_released("LMB")):
+		finalSwipePos = event.get_position()
+		handleSwipe()
 
 # Se mueve la posicion a la nueva mediante un lerp.
 func _process(delta):
 	global_position = global_position.lerp(targetPos.global_position, delta*lerpSpeed)
+
+# Calcula la direccion del swipe
+func handleSwipe():
+	if(initSwipePos.x-finalSwipePos.x>0):
+		moveLeft()
+	else:
+		moveRight()
 
 # Se mueve a la izquierda el jugador si es posible
 func moveLeft():
