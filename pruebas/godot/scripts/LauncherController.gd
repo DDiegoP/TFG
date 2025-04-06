00d167dfe4c2bdc08a829d35d7332ebe21ed8c28
Diego López Balboa <diegol08@ucm.es>
2025-04-06 20:53:05 +0200

    Control con magnetómetro

--- pruebas/godot/scripts/LauncherController.gd
+++ pruebas/godot/scripts/LauncherController.gd
@@ -1,16 +1,36 @@
 extends RigidBody2D
 
+const NORTH_VALUE = 30.0
+
+const SOUTH_VALUE = -25.0
+
 @export var speed : float
+
+@export var rsManager : ReaSensorManager
+
 var currSpeed = 0
+
+var auxMg = 0.0
 
 func _ready():
 	set_gravity_scale(0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	angular_velocity = currSpeed
-	#print(rotation_degrees)
-
+	
+	var mgValue = rsManager.getInteractionAt(0).sensorValue
+	var angle = 0
+	if(mgValue):
+		var x = mgValue.x
+		var y = mgValue.y
+		if (atan2(y, x) >= 0):
+			angle = atan2(y, x) * (180 / PI);
+		else:
+			angle = (atan2(y, x) + 2 * PI) * (180 / PI);
+		
+		angle = deg_to_rad(angle)
+		
+		rotation = lerp_angle(rotation, angle, delta)
 
 func _on_button_left_button_down():
 	currSpeed = -speed
